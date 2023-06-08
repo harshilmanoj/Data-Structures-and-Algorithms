611afc1e22810520fc3d3df5d1a159258557871a
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-06-09 03:33:52 +0530

    Binary Search

--- Searching/Binary Search/binarySearch.java
+++ Searching/Binary Search/binarySearch.java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
 
 public class binarySearch {
+    public static void binarySearchFunction(int arr[],int x){
+        int low = 0;
+        int high = arr.length -1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]==x){
+                System.out.println("The element is present at index : "+ mid);
+                return;
+            }
+            else if(arr[mid]>x){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        System.out.println("The element doesn't exists in the given array.");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -12,5 +32,12 @@ public class binarySearch {
         for(int i = 0; i<n;i++){
             arr[i] = sc.nextInt();
         }
+
+        System.out.println("Enter the element that needed to be searched : ");
+        int x = sc.nextInt();
+
+        binarySearchFunction(arr,x);
+
+        sc.close();
     }
 }
