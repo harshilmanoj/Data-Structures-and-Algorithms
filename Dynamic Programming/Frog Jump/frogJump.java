33e9bfae2e00a5597c77b260a48d4dcc49a45296
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-06-22 23:53:28 +0530

    Frog Jump

--- Dynamic Programming/Frog Jump/frogJump.java
+++ Dynamic Programming/Frog Jump/frogJump.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class frogJump {
+    public static int recursion(int arr[],int i,int dp[]){
+        if(i >= arr.length){
+
+        }
+
+        return 1;
+    }
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] =  new int[n];
+
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int dp[] = new int[n];
+
+        recursion(arr,0,dp);
+        
     }
 }
