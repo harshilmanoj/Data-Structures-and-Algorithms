3f4120c42efb7f6a48e4703de8e51b6108dfed2b
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-06-24 23:53:00 +0530

    Streak

--- Dynamic Programming/Frog Jump/frogJump.java
+++ Dynamic Programming/Frog Jump/frogJump.java
@@ -1,24 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class frogJump {
     public static int recursion(int arr[],int i,int dp[]){
-        if(i >= arr.length){
-
+        if(i == arr.length-1){
+            return 0;
         }
+        if(i = arr.length-2){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        
+        int oneStep = Math.abs(arr[i]-arr[i+2]) + recursion(arr,i+1,dp);
+        int twoStep = Math.abs(arr[i]-arr[i+1]) + recursion(arr,i+2,dp);
 
-        return 1;
+        return dp[i]=Math.min(oneStep, twoStep);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        System.out.println("Enter the no. elements in the array : ");
         int arr[] =  new int[n];
 
+        System.out.println("Enter the element of the array : ");
         for(int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
         }
 
         int dp[] = new int[n];
-        for(inti i =0)
+        Arrays.fill(dp,-1);
 
         recursion(arr,0,dp);
         
