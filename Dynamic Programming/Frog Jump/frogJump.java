e37ebf6cf2fd4cf912d858222f07a2bfe9481d0d
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-07-09 23:57:39 +0530

    Streak

--- Dynamic Programming/Frog Jump/frogJump.java
+++ Dynamic Programming/Frog Jump/frogJump.java
@@ -22,6 +22,7 @@ public class frogJump {
         
         return dp[i]=Math.min(oneStep, twoStep);
         
+        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
