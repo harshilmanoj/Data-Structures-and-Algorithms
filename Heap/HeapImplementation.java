23d33686a0091310d308f73f8245ae9a627e3628
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-09-04 23:13:10 +0530

    Heap3

--- Heap/HeapImplementation.java
+++ Heap/HeapImplementation.java
@@ -21,9 +21,38 @@ public class HeapImplementation {
             return 2*i+2;
         }
 
-        
+        public boolean isLeaf(int i){
+            return (i>currHeapSize/2 && i<=currHeapSize)?true : false;
+        }
 
-        public void maxHeapify(){
+        public void swap(int i, int j){
+            int temp = arr[i];
+            arr[i] =  arr[j];
+            arr[j] = temp;
+        }
+
+        public void maxHeapify(int i){
+            if(isLeaf(i)){
+                return ;
+            }
+            int lc = leftChild(i);
+            int rc = rightChild(i);
+
+            if(arr[i]<arr[lc] || arr[i]<arr[rc]){
+                if(lc>rc){
+                    swap(i,lc);
+                    maxHeapify(lc);
+                }
+                else{
+                    swap(i,rc);
+                    maxHeapify(rc);
+
+                }
+            }
+        }
+
+
+        public void insert(int val){
 
         }
         
