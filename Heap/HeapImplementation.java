d18c0d6815ad4de2c620ffd62fdc850fc62ae2ba
Harshil Manoj <harshil.2020@vitstudent.ac.in>
2023-09-04 23:00:20 +0530

    Heap2

--- Heap/HeapImplementation.java
+++ Heap/HeapImplementation.java
@@ -1,7 +1,31 @@
 package Heap;
 public class HeapImplementation {
     class Heap{
+        int maxHeapSize;
         int arr[];
+        int currHeapSize;
+
+        public Heap(int maxHeapSize){
+            this.maxHeapSize = maxHeapSize;
+            arr= new int[maxHeapSize];
+            currHeapSize = 0;  
+        }
+
+        public int parent(int i){
+            return (i-1)/2;
+        }
+        public int leftChild(int i){
+            return 2*i+1;
+        }
+        public int rightChild(int i){
+            return 2*i+2;
+        }
+
+        
+
+        public void maxHeapify(){
+
+        }
         
     }
     public static void main(String args[]){
